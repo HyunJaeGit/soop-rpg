0dee7614fa748caaf21849048e0c2f5308315e78
guswo1118 <guswo1118@naver.com>
2025-12-22 16:28:25 +0900

    feat : SOOP 알피지 프로젝트 생성

---
+++ src/main/java/com/soop/soop_rpg/SoopRpgApplication.java
@@ -0,0 +1,13 @@
+package com.soop.soop_rpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoopRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoopRpgApplication.class, args);
+	}
+
+}
